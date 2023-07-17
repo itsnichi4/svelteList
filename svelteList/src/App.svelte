1cd9488d8377f8869d4205576731a42e61af6336
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-07-17 13:07:20 +0300

    create svelte template with vite create toDoList html structure add 'add task' button + functionality add #each (checkbox, span, remove btn) + implement functionality add css to check tasks once finished

---
+++ svelteList/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  let newItem = '';
+
+  let todoList = [{text: 'task1', checked: true},
+                  {text: 'task2', checked: false},
+                  {text: 'task3', checked: false}];
+
+function addToList() {
+  todoList = [...todoList, {text: newItem, checked: false}];
+  newItem = '';
+}
+
+function removeFromList(index) {
+  todoList.splice(index, 1)
+  todoList = todoList;
+  }
+</script>
+
+<input bind:value={newItem} type="text" placeholder="new todo item..">
+<button on:click={addToList}>Add</button>
+
+<br/>
+{#each todoList as item, index}
+
+<input bind:checked={item.checked} type="checkbox">
+<span class:checked={item.checked}>{item.text}</span>
+<button on:click={() => removeFromList(index)}>remove</button>
+<br/>
+
+{/each} 
+
+
+
+<style>
+	.checked {
+        text-decoration: line-through;
+    }
+</style>
